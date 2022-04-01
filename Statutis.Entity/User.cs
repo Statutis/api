a31f3d32d7f3cbda4c6e1ccebbf1bcfd9754560e
Ludwig SILVAIN <ludwig@silvain.eu>
2022-04-01 19:18:10 +0200

    feat : classe Utilisateur ref #3

---
+++ Statutis.Entity/User.cs
@@ -0,0 +1,31 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace Statutis.Entity;
+
+public class User
+{
+	public User(string email, string username, string password)
+	{
+		this.Email = email;
+		this.Username = username;
+		this.Password = password;
+		this.Avatar = null;
+	}
+
+	[StringLength(maximumLength: 50), Required]
+	public String Email { get; set; }
+
+	[StringLength(maximumLength: 30), Required]
+	public String Username { get; set; }
+
+	[StringLength(maximumLength: 255), Required]
+	public String Password { get; set; }
+
+	public byte[]? Avatar { get; set; } = null;
+
+	public List<String> Roles { get; set; } = new List<string>();
+	
+	
+
+
+}
