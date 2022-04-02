4e88bfd8c6c422c8507d3fb8cdda4c296314a8c3
Ludwig SILVAIN <ludwig@silvain.eu>
2022-04-02 14:38:27 +0200

    feat : attributs pour FK de EF Core ref #3

--- Statutis.Entity/Service/Group.cs
+++ Statutis.Entity/Service/Group.cs
@@ -16,14 +16,18 @@ public class Group
 		MainGroup = mainGroup;
 	}
 
+	public Guid GroupId;
+
 	[StringLength(maximumLength: 30), Required]
 	public String Name { get; set; }
 
 	public String Description { get; set; }
 
+	public Guid? MainGroupId { get; internal set; } = null;
 	public Group? MainGroup { get; set; } = null;
 
 	public List<Team> Teams { get; set; } = new List<Team>();
-	
+
 	public List<Service> Services { get; set; } = new List<Service>();
+	public List<Group> Children { get; set; } = new List<Group>();
 }
--- Statutis.Entity/Service/Service.cs
+++ Statutis.Entity/Service/Service.cs
@@ -7,9 +7,11 @@ public abstract class Service
 	[StringLength(maximumLength: 30), Required]
 	public String Name { get; set; }
 
-	public String Description { get; set; }
-	
+	public Guid GroupId { get; internal set; }
+
 	public Group Group { get; set; }
+
+	public String Description { get; set; }
 
 	[StringLength(maximumLength: 128), Required]
 	public String Host { get; set; }
