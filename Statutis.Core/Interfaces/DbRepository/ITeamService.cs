f3832b69eaf0dcec46b48862fae91861b7d91bf5
Simon HEBAN <simon.heban@ferenost.fr>
2022-04-05 16:44:03 +0200

    feat: add Team Service Interface

---
+++ Statutis.Core/Interfaces/DbRepository/ITeamService.cs
@@ -0,0 +1,19 @@
+using Statutis.Entity;
+
+namespace Statutis.Core.Interfaces.DbRepository;
+
+public interface ITeamService
+{
+    //Get
+    List<Team> GetAll();
+    Team Get(Guid guid);
+    Team Get(string name);
+
+    List<Team> GetTeamsOfUser(User user);
+    
+    //Update
+    Team Update(Team team);
+    
+    //delete
+    void Delete(Team team);
+}
