affab3cd9361597a565184fd3db5a1c79eeadfed
Simon HEBAN <simon.heban@ferenost.fr>
2022-04-05 16:40:35 +0200

    feat: add User Service interface

---
+++ Statutis.Core/Interfaces/DbRepository/IUserService.cs
@@ -0,0 +1,18 @@
+using Statutis.Entity;
+
+namespace Statutis.Core.Interfaces.DbRepository;
+
+public interface IUserService
+{
+    //Get
+    User GetAll();
+    User Get(int id);
+    User GetByEmail(string email);
+    User GetByUsername(string email);
+    
+    //Update
+    User Update(User user);
+    
+    //delete
+    void Delete(User user);
+}
