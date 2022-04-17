c41572321fb26b7fde056bcf5df1675310a66d9c
Ludwig SILVAIN <ludwig@silvain.eu>
2022-04-17 18:42:05 +0200

    feat : implementation service pour les types de services ref #27

--- Statutis.Core/Interfaces/Business/Service/IServiceTypeService.cs
+++ Statutis.Core/Interfaces/Business/Service/IServiceTypeService.cs
@@ -2,18 +2,18 @@ namespace Statutis.Core.Interfaces.Business.Service;
 
 public interface IServiceTypeService
 {
-    //get
-    Task<List<Entity.Service.ServiceType>> GetAll();
+	//get
+	Task<List<Entity.Service.ServiceType>> GetAll();
 
-    Task<Entity.Service.ServiceType> Get(string name);
-    
-    //insert
-    Task<Entity.Service.ServiceType> Insert(Entity.Service.ServiceType serviceType);
-    
-    //Update
-    Task<Entity.Service.ServiceType> Update(Entity.Service.ServiceType serviceType);
-    
-    //Delete
-    Task<Entity.Service.ServiceType> Delete(Entity.Service.ServiceType serviceType);
+	Task<Entity.Service.ServiceType?> Get(string name);
+
+	//insert
+	Task<Entity.Service.ServiceType> Insert(Entity.Service.ServiceType serviceType);
+
+	//Update
+	Task<Entity.Service.ServiceType> Update(Entity.Service.ServiceType serviceType);
+
+	//Delete
+	void Delete(Entity.Service.ServiceType serviceType);
 
 }
