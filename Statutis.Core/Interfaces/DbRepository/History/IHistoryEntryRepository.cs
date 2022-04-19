961da843558107eed97187014cdff93285f278c8
Ludwig SILVAIN <ludwig@silvain.eu>
2022-04-19 17:02:49 +0200

    feat : interface pour le repository de HistoryEntry ref #38

---
+++ Statutis.Core/Interfaces/DbRepository/History/IHistoryEntryRepository.cs
@@ -0,0 +1,38 @@
+using Statutis.Entity.History;
+
+namespace Statutis.Core.Interfaces.DbRepository.History;
+
+public interface IHistoryEntryRepository
+{
+	/// <summary>
+	/// Dernière entré pour ce service
+	/// </summary>
+	/// <param name="service"></param>
+	/// <returns></returns>
+	public Task<HistoryEntry?> GetLast(Entity.Service.Service service);
+
+	/// <summary>
+	/// Dernière entré avec cet état pour ce service  
+	/// </summary>
+	/// <param name="service"></param>
+	/// <param name="state"></param>
+	/// <returns></returns>
+	public Task<HistoryEntry?> GetLast(Entity.Service.Service service, HistoryState state);
+
+	/// <summary>
+	/// Récupéraion des entrées selon un service (par ordre décroissant)
+	/// </summary>
+	/// <param name="service"></param>
+	/// <param name="count">Nombre limite d'entrée</param>
+	/// <returns></returns>
+	public Task<List<HistoryEntry>> Get(Entity.Service.Service service, int count = 15);
+
+	/// <summary>
+	/// Ajout d'une entrée
+	/// </summary>
+	/// <param name="historyEntry"></param>
+	/// <returns></returns>
+	public Task<List<HistoryEntry>> Add(HistoryEntry historyEntry);
+
+
+}
