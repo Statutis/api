38b54d4d80af6493db36f2a947fdaa03b400cb34
Ludwig SILVAIN <ludwig@silvain.eu>
2022-04-19 20:46:05 +0200

    feat : récupération de l'état général

---
+++ Statutis.API/Controllers/ServiceController.cs
@@ -0,0 +1,26 @@
+using Microsoft.AspNetCore.Mvc;
+using Statutis.API.Models;
+using Statutis.Core.Interfaces.Business.History;
+using Statutis.Entity.History;
+
+namespace Statutis.API.Controllers;
+
+[Route("api/services")]
+[ApiController]
+public class ServiceController : Controller
+{
+	private IHistoryEntryService _historyEntryService;
+
+	public ServiceController(IHistoryEntryService historyEntryService)
+	{
+		_historyEntryService = historyEntryService;
+	}
+
+	[HttpGet, Route("state")]
+	[ProducesResponseType(StatusCodes.Status200OK, Type = typeof(MainStateModel))]
+	public async Task<IActionResult> GetAll()
+	{
+		var res =await _historyEntryService.GetMainState();
+		return Ok(new MainStateModel(){State = res.Item1, LastUpdate = res.Item2});
+	}
+}
