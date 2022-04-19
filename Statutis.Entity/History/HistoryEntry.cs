8f5be9d28f7371ba03e93a879f146755d5002b67
Ludwig SILVAIN <ludwig@silvain.eu>
2022-04-19 16:56:22 +0200

    feat : entité pour l'historique ref #38

---
+++ Statutis.Entity/History/HistoryEntry.cs
@@ -0,0 +1,39 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace Statutis.Entity.History;
+
+public class HistoryEntry
+{
+	[Required]
+	public Service.Service Service { get; set; }
+
+	[Required]
+	public DateTime DateTime { get; set; }
+
+	[Required]
+	public HistoryState State { get; set; } = HistoryState.Unknown;
+
+	public String? message { get; set; } = null;
+
+	public HistoryEntry(Service.Service service) : this(service, System.DateTime.Now)
+	{
+
+	}
+
+	public HistoryEntry(Service.Service service, DateTime dateTime)
+	{
+		this.Service = service;
+		this.DateTime = dateTime;
+	}
+
+	public HistoryEntry(Service.Service service, DateTime dateTime, HistoryState state) : this(service, dateTime)
+	{
+		this.State = state;
+	}
+
+	public HistoryEntry(Service.Service service, DateTime dateTime, HistoryState state, String? message) : this(service, dateTime, state)
+	{
+		this.message = message;
+	}
+
+}
