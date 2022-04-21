5d6fc6e0dd0dc9146540515481f9e311659757b3
Ludwig SILVAIN <ludwig@silvain.eu>
2022-04-21 17:00:32 +0200

    fix : user constoller utilisation d'un model

--- Statutis.API/Models/UserModel.cs
+++ Statutis.API/Models/UserModel.cs
@@ -1,13 +1,30 @@
+using Microsoft.AspNetCore.Mvc;
+using Statutis.Entity;
+
 namespace Statutis.API.Models;
 
-public class UserModel<T>
+public class UserModel
 {
-    public UserModel(T data, bool res)
-    {
-        Data = data;
-        Res = res;
-    }
 
-    public T Data { get; set; }
-    public bool Res { get; set; }
+	public String Email { get; set; }
+
+	public String Username { get; set; }
+
+	public String? AvatarRef { get; set; } = null;
+
+	public List<String> Roles { get; set; } = new List<string>();
+
+	public List<String> TeamsRef { get; set; } = new List<String>();
+
+	public UserModel(User user, IUrlHelper url)
+	{
+		Email = user.Email;
+		Username = user.Username;
+		// Avatar = user.Avatar; TODO Metter la ref vers l'avatar
+		Roles.Add(user.Roles);
+
+		TeamsRef = user.Teams.Select(x => url.Action("GetGuid", "Team", new { guid = x.TeamId }) ?? String.Empty).ToList();
+
+	}
+
 }
