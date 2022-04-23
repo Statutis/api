d18f2dc823794947fdb90a47ca336b555d9f8654
Ludwig SILVAIN <ludwig@silvain.eu>
2022-04-23 16:59:35 +0200

    feat : migration pour nom et prénom des utilisateurs

---
+++ Statutis.DbRepository/Migrations/20220423145645_AddUserNameFirstname.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Statutis.DbRepository.Migrations
+{
+    public partial class AddUserNameFirstname : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Firstname",
+                table: "User",
+                type: "character varying(50)",
+                maxLength: 50,
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Name",
+                table: "User",
+                type: "character varying(50)",
+                maxLength: 50,
+                nullable: true);
+
+            migrationBuilder.UpdateData(
+                table: "User",
+                keyColumn: "Email",
+                keyValue: "contact@silvain.eu",
+                columns: new[] { "Firstname", "Name" },
+                values: new object[] { "Super", "Administrateur" });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Firstname",
+                table: "User");
+
+            migrationBuilder.DropColumn(
+                name: "Name",
+                table: "User");
+        }
+    }
+}
