7bba5d0e76370a179813bc0a90d41ae94397c65d
Ludwig SILVAIN <ludwig@silvain.eu>
2022-04-28 17:55:00 +0200

    fix : copy du dossier content

---
+++ Statutis.API/Form/AtlassianStatusPageForm.cs
@@ -0,0 +1,9 @@
+namespace Statutis.API.Form;
+
+/// <summary>
+/// Fomulaire pour les service avec mode de vérification AtlassianStatusPage
+/// </summary>
+public class AtlassianStatusPageForm : ServiceForm
+{
+	public String JsonUrl { get; set; }
+}
